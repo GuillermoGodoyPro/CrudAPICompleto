a74d4f9075258b0ec270a117d6ed7efd5fc8012e
guillermoGodoyPro <guillermo.rgo92@gmail.com>
2025-04-07 13:36:41 -0300

    202504071336

---
+++ CrudAPI/Controllers/PerfilController.cs
@@ -0,0 +1,29 @@
+﻿using CrudAPI.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CrudAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PerfilController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public PerfilController(AppDbContext context        )
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("lista")]
+        public async Task<ActionResult> Get()
+        {
+            var listaPerfil = await _context.Perfiles.ToListAsync();
+            return Ok(listaPerfil);
+        }
+
+    }
+}
